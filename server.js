751a168a901cedc53555052a3657095be829374a
Batraisk <batraisk@gmail.com>
2021-07-13 01:58:44 +0300

    change proxy package

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 //Install express server
 const express = require('express');
 const path = require('path');
-var proxy = require('express-http-proxy');
+// var proxy = require('express-http-proxy');
+const createProxyMiddleware = require('http-proxy-middleware');
 const app = express();
 
-app.use('/api', proxy('https://submaster-api.herokuapp.com/'));
+app.use('/api', createProxyMiddleware({ target: 'https://submaster-api.herokuapp.com/', changeOrigin: true }));
+
+// app.use('http://localhost:4200/api', proxy('https://submaster-api.herokuapp.com/'));
 // Serve only the static files form the dist directory
 app.use(express.static('./dist/'));
 
@@ -13,4 +16,5 @@ app.get('/*', (req, res) =>
 );
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+// app.listen(process.env.PORT || 8080);
+app.listen(4200);
